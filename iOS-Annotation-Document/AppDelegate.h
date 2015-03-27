3988e9c8775be5c801b6d112acb4b208824f2e9a
aster0id <aster0id@sina.com>
2015-03-27 10:15:07 +0800

    git init

---
+++ iOS-Annotation-Document/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-Annotation-Document
+//
+//  Created by 牛萌 on 15/3/27.
+//  Copyright (c) 2015年  Aster0id. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS-Annotation-Document/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-Annotation-Document
+//
+//  Created by 牛萌 on 15/3/27.
+//  Copyright (c) 2015年  Aster0id. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
